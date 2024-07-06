b325c237156587a22c3f57d21fe0eea8c1b3c42e
Smit Joshi <45530965+smit-joshi814@users.noreply.github.com>
2024-07-06 05:33:48 +0000

    spring boot app with devcontaine.json

---
+++ demo/src/main/java/com/example/codespaces/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.codespaces.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@RestController
+class HelloController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello From Code Spaces";
+	}
+
+}
